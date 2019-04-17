a790b492ec678bc504b668bc1b57940d623f3761
Randy Tarampi <rt@randytarampi.ca>
2019-04-17 11:18:59 -0700

    Only define `driver.setImplicitWaitTimeout` once, when we initialize.
    Since it's an implicit timeout, `appium` and `wd` will just spin for up to 5 seconds waiting for the element to appear.

--- __tests__/appium-test-wd.js
+++ __tests__/appium-test-wd.js
@@ -8,7 +8,7 @@ const androidOpt = {
   automationName: 'UiAutomator2',
   deviceName: 'Android Emulator',
   platformVersion: null,
-  app: path.resolve(__dirname, '..', 'android', 'app','build','outputs','apk','release', 'app-release.apk')
+  app: path.resolve(__dirname, '..', 'android', 'app', 'build', 'outputs', 'apk', 'release', 'app-release.apk')
 };
 
 const serverConfig = {
@@ -25,9 +25,9 @@ describe('Create Android session', function () {
     // Connect to Appium server
     driver = await wd.promiseChainRemote(serverConfig);
 
-     // Start the session
-     await driver.init(androidOpt);
-
+    // Start the session
+    await driver.init(androidOpt)
+      .setImplicitWaitTimeout(5000)
   });
 
   after(async function () {
@@ -36,62 +36,50 @@ describe('Create Android session', function () {
 
 
   it('should create and destroy Android sessions', async function () {
-
     // Check that we're running the ApiDemos app by checking package and activity
     const activity = await driver.getCurrentActivity();
     const pkg = await driver.getCurrentPackage();
     assert.equal(`${pkg}${activity}`, 'com.apktest.MainActivity');
-
   });
 
-
   it('Press alert button and press OK', async function () {
-    await driver.setImplicitWaitTimeout(5000);
     console.log('REGULAR BUTTON')
     let button = await driver.elementByAccessibilityId("test-button-1");
     // console.log(button)
     button.click()
 
     // Click alert message button
-    await driver.setImplicitWaitTimeout(2000);
     let okbutton = await driver.element("id", "android:id/button1");
     console.log('OK')
     // console.log(okbutton);
     okbutton.click()
-    await driver.setImplicitWaitTimeout(5000);
   });
 
   it('Press alert button and press Cancel', async function () {
-    await driver.setImplicitWaitTimeout(5000);
     console.log('REGULAR BUTTON')
     let button = await driver.elementByAccessibilityId("test-button-1");
     // console.log(button)
     button.click()
 
     // Click alert message button
-    await driver.setImplicitWaitTimeout(2000);
     let okbutton = await driver.element("id", "android:id/button2");
     console.log('CANCEL')
     // console.log(okbutton);
     okbutton.click()
-    await driver.setImplicitWaitTimeout(5000);
   });
 
 
   it('Press second button and press OK', async function () {
-    await driver.setImplicitWaitTimeout(5000);
     console.log('REGULAR BUTTON')
     let button = await driver.elementByAccessibilityId("test-button-2");
     // console.log(button)
     button.click()
 
     // Click alert message button
-    await driver.setImplicitWaitTimeout(2000);
     let okbutton = await driver.element("id", "android:id/button1");
     console.log('OK')
     // console.log(okbutton);
     okbutton.click()
-    await driver.setImplicitWaitTimeout(5000);
   });
 
   it('Press alert button and press Cancel', async function () {
@@ -102,13 +90,10 @@ describe('Create Android session', function () {
     button.click()
 
     // Click alert message button
-    await driver.setImplicitWaitTimeout(2000);
     let okbutton = await driver.element("id", "android:id/button2");
     console.log('CANCEL')
     // console.log(okbutton);
     okbutton.click()
-    await driver.setImplicitWaitTimeout(5000);
   });
-
 
 });
